243a780476496a63ca8f2ac0f706550859511786
Rohit Gupta <rohitgupta122343@gmail.com>
2026-05-14 07:34:41 -0700

    pertinent information about the changes being committed.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [ tailwindcss(),react()],
+  plugins: [tailwindcss(), react()],
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://perplexity-project-vay7.onrender.com/', // apna backend port
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
