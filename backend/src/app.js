29b1c6bbd37f5f30b7a985f64a203e1c0228ee5d
Rohit Gupta <rohitgupta122343@gmail.com>
2026-05-15 06:36:31 -0700

    added

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,6 @@ import morgan from 'morgan'
 import cors from 'cors'
 import authRouter from './routers/authRouter.js'
 import chatRouter from './routers/chatRouter.js'
-import { sendEmail } from './services/mail.services.js'
 
 
 const app = express()
@@ -22,25 +21,7 @@ app.get("/", (req, res) => {
   res.send("API is running 🚀");
 });
 
-app.get("/test-mail", async (req, res) => {
 
-  try {
-
-    await sendEmail({
-      to: "yourmobilegmail@gmail.com",
-      subject: "Mobile Test",
-      html: "<h1>Email working on mobile 🚀</h1>"
-    })
-
-    res.send("EMAIL SENT")
-
-  } catch (err) {
-
-    console.log(err)
-
-    res.send("FAILED")
-  }
-})
 
 app.use('/api/auth',authRouter)
 app.use('/api/chats',chatRouter)
