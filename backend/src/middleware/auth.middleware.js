4542dbf779c03aaa49cb7d026582e84b62105e0d
Rohit Gupta <rohitgupta122343@gmail.com>
2026-05-15 03:07:32 -0700

    add

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,27 +1,32 @@
-
 import jwt from 'jsonwebtoken'
 
+export async function authUser(req, res, next) {
 
-export async function authUser(req,res,next){
-    
     const token = req.cookies.token
-    
-        if(!token){
-            return res.status(402).json({
-                message : 'token not provided'
-            })
-        }
+
+    if (!token) {
+        return res.status(401).json({
+            message: 'Token not provided'
+        })
+    }
 
     try {
-            const decoded = jwt.verify(token,process.env.JWT_SECRECT)
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRECT
+        )
 
         req.user = decoded
 
         next()
-        } catch (error) {
-            return res.status(401).json({
-                message : 'Unauthorized',
-                err : 'Invalid Token'
-            })
-        }
+
+    } catch (error) {
+
+        return res.status(401).json({
+            message: 'Unauthorized',
+            err: 'Invalid Token'
+        })
+
+    }
 }
