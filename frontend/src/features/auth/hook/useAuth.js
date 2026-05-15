4366247fadaaa473b5f83fd4434c5263aa3715e8
Rohit Gupta <rohitgupta122343@gmail.com>
2026-05-15 00:16:19 -0700

    added console log for chat data and fixed user existence check in auth controller.

--- frontend/src/features/auth/hook/useAuth.js
+++ frontend/src/features/auth/hook/useAuth.js
@@ -64,7 +64,7 @@ export  function useAuth(){
             dispatch(setloading(true))
             await logout()
             dispatch(setuser(null))
-             <Navigate to='/login'/>
+             
         } catch (error) {
             dispatch(seterror(error))
         } finally {
