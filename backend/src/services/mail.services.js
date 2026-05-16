c18088cef2c395d2b0e590d0490d919feb1f6f6d
Rohit Gupta <rohitgupta122343@gmail.com>
2026-05-16 07:16:12 -0700

    added

--- backend/src/services/mail.services.js
+++ backend/src/services/mail.services.js
@@ -3,82 +3,80 @@ dotenv.config()
 
 
 
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
 
 
-const transporter = nodemailer.createTransport({
+// const transporter = nodemailer.createTransport({
 
-  service: "gmail",
+//   service: "gmail",
 
-  auth: {
-  user: process.env.GOOGLE_USER,
-  pass: process.env.GOOGLE_APP_PASSWORD,
-},
-});
+//     port: 587,
+//   secure: false,
 
-transporter.verify()
-  .then(() => {
-    console.log("Email transporter is ready");
-  })
-  .catch((err) => {
-    console.log("Transporter Error:", err);
-  });
+//   family: 4, // ⭐ force IPv4
 
-export async function sendEmail({ to, subject, html }) {
+//   auth: {
+//   user: process.env.GOOGLE_USER,
+//   pass: process.env.GOOGLE_APP_PASSWORD,
+// },
+// });
 
-  try {
-
-    const mailOptions = {
-      from: process.env.GOOGLE_USER,
-      to,
-      subject,
-      html,
-    };
-
-    const details = await transporter.sendMail(mailOptions);
-
-    console.log("Email Sent:", details);
-
-    return details;
-
-  } catch (err) {
-
-    console.log("EMAIL ERROR:", err);
-
-    throw err;
-  }
-}
-
-
-// import dotenv from 'dotenv'
-// dotenv.config()
-
-// import { Resend } from 'resend'
-
-// const resend = new Resend(process.env.RESEND_API_KEY)
+// transporter.verify()
+//   .then(() => {
+//     console.log("Email transporter is ready");
+//   })
+//   .catch((err) => {
+//     console.log("Transporter Error:", err);
+//   });
 
 // export async function sendEmail({ to, subject, html }) {
 
 //   try {
 
-//     const data = await resend.emails.send({
-
-//       from: 'onboarding@resend.dev',
-
+//     const mailOptions = {
+//       from: process.env.GOOGLE_USER,
 //       to,
-
 //       subject,
+//       html,
+//     };
 
-//       html
-//     })
+//     const details = await transporter.sendMail(mailOptions);
 
-//     console.log("✅ EMAIL SENT:", data)
+//     console.log("Email Sent:", details);
 
-//     return data
+//     return details;
 
 //   } catch (err) {
 
-//     console.log("❌ EMAIL ERROR:", err)
+//     console.log("EMAIL ERROR:", err);
+
+//     throw err;
 //   }
 // }
+
+
+// import dotenv from 'dotenv'
+// dotenv.config()
+
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function sendEmail({ to, subject, html }) {
+  try {
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev", // testing domain
+      to,
+      subject,
+      html,
+    });
+
+    console.log("EMAIL SENT:", data);
+    return data;
+
+  } catch (err) {
+    console.log("EMAIL ERROR:", err);
+    throw err;
+  }
+}
