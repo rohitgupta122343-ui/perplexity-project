b87b3ae6ef5a5c34366da9388798936dcb5d1210
Rohit Gupta <rohitgupta122343@gmail.com>
2026-05-16 07:05:09 -0700

    railway-deploy

--- backend/src/services/mail.services.js
+++ backend/src/services/mail.services.js
@@ -5,26 +5,16 @@ dotenv.config()
 
 import nodemailer from "nodemailer";
 
+
+
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
 
-  port: 587,
-
-  secure: false,
-
-  family: 4,
+  service: "gmail",
 
   auth: {
-    type: "OAuth2",
-
-    user: process.env.GOOGLE_USER,
-
-    clientId: process.env.GOOGLE_CLIENT_ID,
-
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-
-    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-  },
+  user: process.env.GOOGLE_USER,
+  pass: process.env.GOOGLE_APP_PASSWORD,
+},
 });
 
 transporter.verify()
