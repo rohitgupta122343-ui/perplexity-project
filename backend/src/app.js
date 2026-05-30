65097fae7f5eaa933878385b228ecd97e4e85887
Rohit Gupta <rohitgupta122343@gmail.com>
2026-05-30 15:20:03 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,10 +12,7 @@ const app = express()
 app.use(express.json())  
 app.use(cookieParser())
 app.use(morgan("dev"))
-app.use(cors({
-    origin: 'https://perplexity-project-navy.vercel.app',
-    credentials: true
-}))
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
